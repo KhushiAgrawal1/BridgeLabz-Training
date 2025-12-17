93112712bcde94cde9a0447748036279e68f6b0e
Khushi Agrawal <agrawalkhushi267@gmail.com>
2025-12-17 19:13:16 +0530

    [KhushiAgrawal] Added : Program to calculate Area of Triangle in sq. inch and sq. cm

---
+++ gcr-codebase/programming-elements/Level 2/TriangleArea.java
@@ -0,0 +1,24 @@
+// Program to calculate area of a triangle
+
+import java.util.Scanner;
+
+public class TriangleArea
+{
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+
+        // Read base and height in cm
+        double base = input.nextDouble();
+        double height = input.nextDouble();
+
+        // Area in square centimeters
+        double areaSqCm = 0.5 * base * height;
+
+        // Convert to square inches
+        double areaSqIn = areaSqCm / (2.54 * 2.54);
+
+        // Display result
+        System.out.println("The Area of the triangle in sq in is " + areaSqIn + " and sq cm is " + areaSqCm);
+    }
+}
