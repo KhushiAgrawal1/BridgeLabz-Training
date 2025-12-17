46683235710a7a7ef75a72889ead5d81e814f707
Khushi Agrawal <agrawalkhushi267@gmail.com>
2025-12-17 15:56:40 +0530

    [Khushi agrawal] : adding age of Hurry

---
+++ gcr-codebase/programming-elements/Level-one/AgeOfHarry.java
@@ -0,0 +1,11 @@
+public class AgeOfHarry{
+    public static void main(String agrs[]){
+
+        int birthyear = 2000;
+        int currentYear = 2024;
+
+        int age = currentYear - birthyear;
+
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
