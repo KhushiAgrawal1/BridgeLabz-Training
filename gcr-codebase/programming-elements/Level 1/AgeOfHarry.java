5ccc7eda66dfba8ba9ddfc00b04d2dd961909f3b
Khushi Agrawal <agrawalkhushi267@gmail.com>
2025-12-17 18:05:40 +0530

    <KhushiAgrawal] Added : Find Age of Harry

---
+++ gcr-codebase/programming-elements/Level 1/AgeOfHarry.java
@@ -0,0 +1,12 @@
+public class AgeOfHarry{
+    public static void main(String agrs[]){
+
+        int birthyear = 2000;
+        int currentYear = 2024;
+
+        int age = currentYear - birthyear;
+
+	//display result
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
