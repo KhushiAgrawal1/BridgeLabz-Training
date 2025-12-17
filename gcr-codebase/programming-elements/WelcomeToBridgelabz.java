31a2b4fb64562b573bf7be5189a84cb70dc27283
Khushi Agrawal <agrawalkhushi267@gmail.com>
2025-12-17 18:55:48 +0530

    [Khushi Agrawal] Added : Program to print a statement

--- gcr-codebase/programming-elements/WelcomeToBridgelabz.java
+++ gcr-codebase/programming-elements/WelcomeToBridgelabz.java
@@ -1,5 +1,9 @@
+//printing statement
+
 public class WelcomeToBridgelabz{
-public static void main(String args[]){
-System.out.println("Welcome To Bridgelabz!!!!!!!");
-}
+    public static void main(String args[]){
+
+        System.out.println("Welcome To Bridgelabz!");
+
+    }
 }
