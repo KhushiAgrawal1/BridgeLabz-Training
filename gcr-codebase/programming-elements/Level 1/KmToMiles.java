472cfdfdfafa3ec446f8669bfcf92d3e5c7b2c36
Khushi Agrawal <agrawalkhushi267@gmail.com>
2025-12-17 18:10:12 +0530

    [Khushi Agrawal] Added : Conversion from Kilometers to Miles

---
+++ gcr-codebase/programming-elements/Level 1/KmToMiles.java
@@ -0,0 +1,16 @@
+// Program to convert distance from kilometers to miles
+
+public class KmToMiles
+{
+    public static void main(String args[])
+    {
+        // Distance in kilometers
+        double km = 10.8;
+
+        // Conversion from km to miles
+        double miles = km / 1.6;
+
+        // Display result
+        System.out.println("The distance " + km + " km in miles is " + miles);
+    }
+}
