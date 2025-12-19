3a1fcb091ba3ee6ef84f79c21071f3940999895c
Khushi Agrawal <agrawalkhushi267@gmail.com>
2025-12-19 14:29:53 +0530

    [KhushiAgrawal] Added : Multiplication Table ( 6 to 9 )

---
+++ gcr-codebase/arrays/Level 1/SixToNineTable.java
@@ -0,0 +1,12 @@
+// Program Name : Multiplication Table ( 6 to 9 )
+// Description : Displays multiplication table from 6 to 9
+
+import java.util.Scanner;
+
+public class SixToNineTable
+{
+    public static void main (String[] args)
+	{
+	    Scanner 
+	}
+}	
