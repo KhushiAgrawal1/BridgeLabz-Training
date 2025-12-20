73c73a84014a8a7addbaa741fc7cdf9ea5d56c77
Khushi Agrawal <agrawalkhushi267@gmail.com>
2025-12-20 18:27:42 +0530

    [KhushiAgrawal] Added : Number Analysis Program

--- gcr-codebase/arrays/Level 1/NumberAnalysis.java
+++ gcr-codebase/arrays/Level 1/NumberAnalysis.java
@@ -1,5 +1,5 @@
 // Program Name : Number Analysis Program
-// Description  : This program checks number type and compares first and last elements of an array
+// Description  : Checks number type and compares first and last elements
 
 import java.util.Scanner;
 
@@ -7,20 +7,26 @@ public class NumberAnalysis
 {
     public static void main(String[] args)
     {
+        // Scanner object for user input
         Scanner input = new Scanner(System.in);
 
+        // Array to store 5 numbers
         int[] numbers = new int[5];
 
+        // Loop to take input values
         for (int i = 0; i < numbers.length; i++)
         {
             System.out.println("Enter number " + (i + 1));
             numbers[i] = input.nextInt();
         }
 
+        // Loop to analyze each number
         for (int i = 0; i < numbers.length; i++)
         {
+            // Check if number is positive
             if (numbers[i] > 0)
             {
+                // Check even or odd
                 if (numbers[i] % 2 == 0)
                 {
                     System.out.println(numbers[i] + " is Positive and Even");
@@ -30,16 +36,19 @@ public class NumberAnalysis
                     System.out.println(numbers[i] + " is Positive and Odd");
                 }
             }
+            // Check if number is negative
             else if (numbers[i] < 0)
             {
                 System.out.println(numbers[i] + " is Negative");
             }
+            // Number is zero
             else
             {
                 System.out.println("Number is Zero");
             }
         }
 
+        // Compare first and last elements of the array
         if (numbers[0] == numbers[4])
         {
             System.out.println("First and Last elements are Equal");
