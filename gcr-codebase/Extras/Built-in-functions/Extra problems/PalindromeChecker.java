07cb33101bf8a704d91f44a9527b07f63eb4e259
Khushi Agrawal <agrawalkhushi267@gmail.com>
2025-12-24 15:49:35 +0530

    [KhushiAgrawal] Added : Palindrome Checker Program

---
+++ gcr-codebase/Extras/Built-in-functions/Extra problems/PalindromeChecker.java
@@ -0,0 +1,36 @@
+// Program Name : PalindromeChecker
+// Description  : Checks if a string is a palindrome
+
+import java.util.Scanner;
+
+public class PalindromeChecker
+{
+    public static boolean isPalindrome(String text)
+    {
+        int start = 0;
+        int end = text.length() - 1;
+
+        while (start < end)
+        {
+            if (text.charAt(start) != text.charAt(end))
+                return false;
+
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String text = sc.nextLine();
+
+        if (isPalindrome(text))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not a Palindrome");
+    }
+}
