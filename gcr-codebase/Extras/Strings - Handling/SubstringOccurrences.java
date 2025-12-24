00d18265f987b90a4a734b68c43b8cf032ec98eb
Khushi Agrawal <agrawalkhushi267@gmail.com>
2025-12-24 15:12:45 +0530

    [KhushiAgrawal] Added : Substring Occurrences in a String Program

---
+++ gcr-codebase/Extras/Strings - Handling/SubstringOccurrences.java
@@ -0,0 +1,24 @@
+/* Program Name: Substring Occurrences
+   Description : This program counts the number of times a substring
+                 occurs in a string using indexOf().
+*/
+import java.util.Scanner;
+
+public class SubstringOccurrences {
+    public static int countOccurrences(String str, String sub) {          
+        int count = 0, index = 0;
+
+        while ((index = str.indexOf(sub, index)) != -1) {
+            count++;
+            index += sub.length();
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String sub = sc.nextLine();
+        System.out.println(countOccurrences(str, sub));
+    }
+}
