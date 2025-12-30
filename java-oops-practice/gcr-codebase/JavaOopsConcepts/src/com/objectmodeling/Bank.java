e2bacfc4dd504ea4e4e99ebf9ec4ec2ce6f272a4
Khushi Agrawal <agrawalkhushi267@gmail.com>
2025-12-30 16:31:32 +0530

    [KhushiAgrawal] Added : Bank Class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/Bank.java
@@ -0,0 +1,14 @@
+package com.objectmodeling;
+
+public class Bank {
+    String bankName;
+
+    Bank(String bankName) {
+        this.bankName = bankName;
+    }
+
+    void openAccount(Customer customer) {
+        System.out.println("Account opened for " + customer.name + " in " + bankName);
+    }
+}
+
