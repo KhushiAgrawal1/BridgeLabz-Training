7ea018abba129fe72d1a36dba5cdcafb4ba53ea6
Khushi Agrawal <agrawalkhushi267@gmail.com>
2025-12-31 10:49:06 +0530

    [KhushiAgrawal] Added : Customer class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/Customer.java
@@ -0,0 +1,16 @@
+package com.objectmodeling;
+
+public class Customer {
+    String name;
+    double balance;
+
+    Customer(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    void viewBalance() {
+        System.out.println(name + "'s balance: ₹" + balance);
+    }
+}
+
