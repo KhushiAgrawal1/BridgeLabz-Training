243cdcf656ffe887b341e7cfa33c85bcabe3720b
Khushi Agrawal <agrawalkhushi267@gmail.com>
2025-12-31 17:00:14 +0530

    [KhushiAgrawal] Added : Book class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/singleinheritance/LibraryManagementSystem/Book.java
@@ -0,0 +1,19 @@
+package com.inheritance.singleinheritance.LibraryManagementSystem;
+
+class Book {
+    String title;
+    int publicationYear;
+
+    // Constructor
+    Book(String title, int publicationYear) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+
+    // Method to display book details
+    void displayInfo() {
+        System.out.println("Book Title: " + title);
+        System.out.println("Publication Year: " + publicationYear);
+    }
+}
+
