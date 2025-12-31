6d97099957293d42e8ec95ac78e210e7fd144497
Khushi Agrawal <agrawalkhushi267@gmail.com>
2025-12-31 17:12:04 +0530

    [KhushiAgrawal] Added : Person class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchicalinheritance/SchoolSystemWithDifferentRoles/Person.java
@@ -0,0 +1,17 @@
+package com.inheritance.hierarchicalinheritance.SchoolSystemWithDifferentRoles;
+
+public class Person {
+    protected String name;
+    protected int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayPersonDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
