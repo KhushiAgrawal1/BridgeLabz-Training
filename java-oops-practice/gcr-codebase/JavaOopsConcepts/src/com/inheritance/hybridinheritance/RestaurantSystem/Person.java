f81730c596edb01c8c2be4ed4da6da67fe69f5a3
Khushi Agrawal <agrawalkhushi267@gmail.com>
2025-12-31 18:34:45 +0530

    [KhushiAgrawal] Added : Person class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/RestaurantSystem/Person.java
@@ -0,0 +1,15 @@
+package com.inheritance.hybridinheritance.RestaurantSystem;
+
+public class Person {
+    protected String name;
+    protected int id;
+
+    public Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name + ", ID: " + id);
+    }
+}
