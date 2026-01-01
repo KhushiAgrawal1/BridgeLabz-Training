8665f46a104b229bc3ac870f6005c6202016e057
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-01 12:59:21 +0530

    [KhushiAgrawal] Added : Electronics class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism_interface_abstractclass/ecommercePlatform/Electronics.java
@@ -0,0 +1,23 @@
+package com.encapsulation_polymorphism_interface_abstractclass.ecommercePlatform;
+
+public class Electronics extends Product implements Taxable {
+
+    public Electronics(int id, String name, double price) {
+        super(id, name, price);
+    }
+
+    @Override
+    public double calculateDiscount() {
+        return getPrice() * 0.10;
+    }
+
+    @Override
+    public double calculateTax() {
+        return getPrice() * 0.18;
+    }
+
+    @Override
+    public String getTaxDetails() {
+        return "GST 18%: ₹" + calculateTax();
+    }
+}
