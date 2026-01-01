2674efb47a385e2cc28f9d28948318ea828926bb
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-01 13:12:36 +0530

    [KhushiAgrawal] Added : Discountable class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism_interface_abstractclass/OnlineFoodDeliverySystem/Discountable.java
@@ -0,0 +1,9 @@
+package com.encapsulation_polymorphism_interface_abstractclass.OnlineFoodDeliverySystem;
+
+public interface Discountable {
+
+    double applyDiscount();
+
+    String getDiscountDetails();
+}
+
