cf9f947110d54b5b2fddb240073c37e27dfaef3a
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-01 13:01:23 +0530

    [KhushiAgrawal] Added : Employee class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism_interface_abstractclass/employeeManagementSystem/Employee.java
@@ -0,0 +1,56 @@
+package com.encapsulation_polymorphism_interface_abstractclass.employeeManagementSystem;
+
+public abstract class Employee implements Department {
+
+    private int employeeId;
+    private String name;
+    protected double baseSalary;
+    private String department;
+
+    // Constructor
+    public Employee(int employeeId, String name, double baseSalary) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.baseSalary = baseSalary;
+    }
+
+    // Getters and Setters (Encapsulation)
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    // Interface methods
+    @Override
+    public void assignDepartment(String deptName) {
+        this.department = deptName;
+    }
+
+    @Override
+    public String getDepartmentDetails() {
+        return department;
+    }
+
+    // Abstract method
+    public abstract double calculateSalary();
+
+    // Concrete method
+    public void displayDetails() {
+        System.out.println("Employee ID : " + employeeId);
+        System.out.println("Name        : " + name);
+        System.out.println("Department  : " + department);
+        System.out.println("Salary      : ₹" + calculateSalary());
+        System.out.println("----------------------------");
+    }
+}
