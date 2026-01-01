c29f82c9b7f26141392cfcc15c7981596fc97084
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-01 13:16:49 +0530

    [KhushiAgrawal] Added : Vehicle class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism_interface_abstractclass/RideHailingApplication/Vehicle.java
@@ -0,0 +1,38 @@
+package com.encapsulation_polymorphism_interface_abstractclass.RideHailingApplication;
+
+public abstract class Vehicle {
+
+    private String vehicleId;
+    private String driverName;
+    protected double ratePerKm;
+
+    public Vehicle(String vehicleId, String driverName, double ratePerKm) {
+        this.vehicleId = vehicleId;
+        this.driverName = driverName;
+        this.ratePerKm = ratePerKm;
+    }
+
+    // Encapsulation
+    public String getVehicleId() {
+        return vehicleId;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public double getRatePerKm() {
+        return ratePerKm;
+    }
+
+    // Abstract method
+    public abstract double calculateFare(double distance);
+
+    // Concrete method
+    public void getVehicleDetails() {
+        System.out.println("Vehicle ID: " + vehicleId);
+        System.out.println("Driver Name: " + driverName);
+        System.out.println("Rate per Km: ₹" + ratePerKm);
+    }
+}
+
