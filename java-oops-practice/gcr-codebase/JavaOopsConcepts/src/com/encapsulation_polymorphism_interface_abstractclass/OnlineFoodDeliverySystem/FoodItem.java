e3fa0f12f3b219dcb82e242f445032bcd97d4b9d
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-01 13:09:30 +0530

    [KhushiAgrawal] Added : FoodItem class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism_interface_abstractclass/OnlineFoodDeliverySystem/FoodItem.java
@@ -0,0 +1,43 @@
+package com.encapsulation_polymorphism_interface_abstractclass.OnlineFoodDeliverySystem;
+
+public abstract class FoodItem {
+
+    private String itemName;
+    private double price;
+    private int quantity;
+
+    public FoodItem(String itemName, double price, int quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // Encapsulation
+    public String getItemName() {
+        return itemName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    protected void updateQuantity(int quantity) {
+        if (quantity > 0) {
+            this.quantity = quantity;
+        }
+    }
+
+    // Abstract method
+    public abstract double calculateTotalPrice();
+
+    // Concrete method
+    public void getItemDetails() {
+        System.out.println("Item: " + itemName);
+        System.out.println("Price: ₹" + price);
+        System.out.println("Quantity: " + quantity);
+    }
+}
