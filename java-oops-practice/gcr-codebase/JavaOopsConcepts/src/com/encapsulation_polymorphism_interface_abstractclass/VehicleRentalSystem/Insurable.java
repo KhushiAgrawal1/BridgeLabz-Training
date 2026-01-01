dd85268fc13388060a8ba14ebb5ed8327381059b
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-01 13:20:23 +0530

    [KhushiAgrawal] Added : Insurable class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism_interface_abstractclass/VehicleRentalSystem/Insurable.java
@@ -0,0 +1,8 @@
+package com.encapsulation_polymorphism_interface_abstractclass.VehicleRentalSystem;
+
+public interface Insurable {
+
+    double calculateInsurance();
+
+    String getInsuranceDetails();
+}
