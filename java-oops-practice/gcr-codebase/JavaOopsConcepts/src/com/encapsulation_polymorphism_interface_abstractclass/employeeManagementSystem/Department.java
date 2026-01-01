c851f7f824a27b59b758943d0a324398e3d2faa5
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-01 13:01:53 +0530

    [KhushiAgrawal] Added : Department class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism_interface_abstractclass/employeeManagementSystem/Department.java
@@ -0,0 +1,6 @@
+package com.encapsulation_polymorphism_interface_abstractclass.employeeManagementSystem;
+
+public interface Department {
+    void assignDepartment(String deptName);
+    String getDepartmentDetails();
+}
