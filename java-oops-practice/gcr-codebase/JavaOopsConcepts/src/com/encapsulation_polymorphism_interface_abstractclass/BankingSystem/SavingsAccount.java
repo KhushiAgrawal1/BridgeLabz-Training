f50cf6a7ca98a0c81ff674b7760b9a5f632b60af
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-01 12:53:28 +0530

    [KhushiAgrawal] Added : SavingsAccount class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism_interface_abstractclass/BankingSystem/SavingsAccount.java
@@ -0,0 +1,23 @@
+package com.encapsulation_polymorphism_interface_abstractclass.BankingSystem;
+
+public class SavingsAccount extends BankAccount implements Loanable {
+
+    public SavingsAccount(String accNo, String name, double balance) {
+        super(accNo, name, balance);
+    }
+
+    @Override
+    public double calculateInterest() {
+        return balance * 0.04; // 4% interest
+    }
+
+    @Override
+    public void applyForLoan(double amount) {
+        System.out.println("Savings Account Loan Applied: ₹" + amount);
+    }
+
+    @Override
+    public boolean calculateLoanEligibility() {
+        return balance >= 50000;
+    }
+}
