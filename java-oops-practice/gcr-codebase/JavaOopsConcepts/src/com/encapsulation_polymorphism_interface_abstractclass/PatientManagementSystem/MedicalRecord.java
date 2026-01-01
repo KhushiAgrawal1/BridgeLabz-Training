519dd408f328b09fba742b7687cf894abc52fc24
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-01 13:15:51 +0530

    [KhushiAgrawal] Added : MedicalRecord class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism_interface_abstractclass/PatientManagementSystem/MedicalRecord.java
@@ -0,0 +1,8 @@
+package com.encapsulation_polymorphism_interface_abstractclass.PatientManagementSystem;
+
+public interface MedicalRecord {
+
+    void addRecord(String diagnosis, String history);
+
+    void viewRecords();
+}
