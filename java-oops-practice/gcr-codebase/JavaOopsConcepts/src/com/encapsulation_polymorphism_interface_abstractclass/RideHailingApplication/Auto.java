21f8862b68891c4ea33b937ffcb58cf97eb16ed9
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-01 13:17:15 +0530

    [KhushiAgrawal] Added : Auto class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism_interface_abstractclass/RideHailingApplication/Auto.java
@@ -0,0 +1,26 @@
+package com.encapsulation_polymorphism_interface_abstractclass.RideHailingApplication;
+
+public class Auto extends Vehicle implements GPS {
+
+    private String currentLocation;
+
+    public Auto(String vehicleId, String driverName, double ratePerKm) {
+        super(vehicleId, driverName, ratePerKm);
+        this.currentLocation = "Unknown";
+    }
+
+    @Override
+    public double calculateFare(double distance) {
+        return (distance * ratePerKm) + 30; // base charge
+    }
+
+    @Override
+    public String getCurrentLocation() {
+        return currentLocation;
+    }
+
+    @Override
+    public void updateLocation(String newLocation) {
+        this.currentLocation = newLocation;
+    }
+}
