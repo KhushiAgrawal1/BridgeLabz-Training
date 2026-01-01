d354ce86d9754a5af99a28d56d66534df9d3b87a
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-01 13:11:17 +0530

    [KhushiAgrawal] Added : VegItem class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation_polymorphism_interface_abstractclass/OnlineFoodDeliverySystem/VegItem.java
@@ -0,0 +1,23 @@
+package com.encapsulation_polymorphism_interface_abstractclass.OnlineFoodDeliverySystem;
+
+public class VegItem extends FoodItem implements Discountable {
+
+    public VegItem(String name, double price, int quantity) {
+        super(name, price, quantity);
+    }
+
+    @Override
+    public double calculateTotalPrice() {
+        return getPrice() * getQuantity();
+    }
+
+    @Override
+    public double applyDiscount() {
+        return calculateTotalPrice() * 0.10; // 10% discount
+    }
+
+    @Override
+    public String getDiscountDetails() {
+        return "Veg Discount (10%) applied";
+    }
+}
