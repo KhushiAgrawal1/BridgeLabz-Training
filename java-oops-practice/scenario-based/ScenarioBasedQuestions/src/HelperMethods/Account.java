c1c16a9e5247a3b573600c47352b7d401b231c98
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-02 16:29:40 +0530

    [KhushiAgrawal] Added : Account class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/HelperMethods/Account.java
@@ -0,0 +1,11 @@
+package com.HelperMethods;
+
+public class Account {
+    double balance;
+    String accountNumber;
+
+    public Account(double balance, String accountNumber) {
+        this.balance = balance;
+        this.accountNumber = accountNumber;
+    }
+}
