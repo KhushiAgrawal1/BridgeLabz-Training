c3461b02b0928d65fad2df31c4eaef7dd5445746
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-07 19:10:53 +0530

    [KhushiAgrawal] Added : TwoSum class

---
+++ dsa-practice/gcr-codebase/JavaDsaConcepts/src/Hash_Map_Hash_Function/TwoSum.java
@@ -0,0 +1,27 @@
+package Hash_Map_Hash_Function;
+
+import java.util.HashMap;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[] arr, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(target - arr[i])) {
+                return new int[]{map.get(target - arr[i]), i};
+            }
+            map.put(arr[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(arr, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+}
+
