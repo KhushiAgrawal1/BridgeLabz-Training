cbb04806959ba06898ca6e2b9b62f14afe0c07b6
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-07 19:15:17 +0530

    [KhushiAgrawal] Added : QuickSortProducts class

---
+++ dsa-practice/gcr-codebase/JavaDsaConcepts/src/Sorting_Algorithms/QuickSortProducts.java
@@ -0,0 +1,43 @@
+package Sorting_Algorithms;
+
+class QuickSortProducts {
+
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int p = partition(arr, low, high);
+            quickSort(arr, low, p - 1);
+            quickSort(arr, p + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {1200, 500, 1500, 800};
+
+        quickSort(prices, 0, prices.length - 1);
+
+        System.out.println("Sorted Product Prices:");
+        for (int p : prices)
+            System.out.print(p + " ");
+    }
+}
+
