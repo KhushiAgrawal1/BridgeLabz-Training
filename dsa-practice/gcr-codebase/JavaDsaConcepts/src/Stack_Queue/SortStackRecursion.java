748e8db8bb14aad1c34335977d8037524b25232b
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-07 17:21:28 +0530

    [KhushiAgrawal] Added : SortStackRecursion class

---
+++ dsa-practice/gcr-codebase/JavaDsaConcepts/src/Stack_Queue/SortStackRecursion.java
@@ -0,0 +1,37 @@
+package Stack_Queue;
+
+import java.util.Stack;
+
+public class SortStackRecursion {
+
+    public static void sortStack(Stack<Integer> stack) {
+        if (!stack.isEmpty()) {
+            int temp = stack.pop();
+            sortStack(stack);
+            insertSorted(stack, temp);
+        }
+    }
+
+    private static void insertSorted(Stack<Integer> stack, int element) {
+        if (stack.isEmpty() || element > stack.peek()) {
+            stack.push(element);
+            return;
+        }
+
+        int temp = stack.pop();
+        insertSorted(stack, element);
+        stack.push(temp);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(30);
+        stack.push(10);
+        stack.push(20);
+
+        sortStack(stack);
+
+        System.out.println(stack);
+    }
+}
+
