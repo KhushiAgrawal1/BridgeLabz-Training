05eaf796c518c599ce08b04cd29ac287ec71629f
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-07 16:52:30 +0530

    [KhushiAgrawal] Added : Vehicle class

---
+++ dsa-practice/scenario-based/ScenarioBasedQuestions/src/CabbyGo_Ride_Hailing_App/Vehicle.java
@@ -0,0 +1,18 @@
+package CabbyGo_Ride_Hailing_App;
+
+public class Vehicle {
+    protected String vehicleNumber;
+    protected int capacity;
+    protected String type;
+
+    public Vehicle(String vehicleNumber, int capacity, String type) {
+        this.vehicleNumber = vehicleNumber;
+        this.capacity = capacity;
+        this.type = type;
+    }
+
+    public void displayVehicle() {
+        System.out.println(type + " - " + vehicleNumber);
+    }
+}
+
