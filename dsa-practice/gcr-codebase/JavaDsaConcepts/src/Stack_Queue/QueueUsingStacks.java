2f9b0fabfdccf3dfb793384a703e94ba21932d34
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-07 17:19:56 +0530

    [KhushiAgrawal] Added : QueueUsingStacks class

---
+++ dsa-practice/gcr-codebase/JavaDsaConcepts/src/Stack_Queue/QueueUsingStacks.java
@@ -0,0 +1,39 @@
+package Stack_Queue;
+
+import java.util.Stack;
+
+public class QueueUsingStacks {
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    // Enqueue operation
+    public void enqueue(int x) {
+        s1.push(x);
+    }
+
+    // Dequeue operation
+    public int dequeue() {
+        if (s1.isEmpty() && s2.isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
+    }
+
+    public static void main(String[] args) {
+        QueueUsingStacks q = new QueueUsingStacks();
+        q.enqueue(10);
+        q.enqueue(20);
+        q.enqueue(30);
+
+        System.out.println(q.dequeue());
+        System.out.println(q.dequeue());
+    }
+}
+
