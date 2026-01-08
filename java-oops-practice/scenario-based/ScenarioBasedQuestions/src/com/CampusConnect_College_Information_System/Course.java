88758215a5eb1a11be543543b2f8f1536663bc37
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-08 14:19:32 +0530

    [KhushiAgrawal] Added : Course class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/CampusConnect_College_Information_System/Course.java
@@ -0,0 +1,30 @@
+package com.CampusConnect_College_Information_System;
+
+import java.util.ArrayList;
+
+public class Course implements ICourseActions {
+
+    private String courseName;
+    private Faculty faculty;
+    private ArrayList<Student> students = new ArrayList<>();
+
+    public Course(String courseName, Faculty faculty) {
+        this.courseName = courseName;
+        this.faculty = faculty;
+    }
+
+    public void enrollCourse(Student student) {
+        students.add(student);
+    }
+
+    public void dropCourse(Student student) {
+        students.remove(student);
+    }
+
+    public void showCourseDetails() {
+        System.out.println("Course: " + courseName);
+        faculty.printDetails();
+        System.out.println("Enrolled Students: " + students.size());
+    }
+}
+
