6a960b7a8ebb1b1a60d3afc3c7150663d8996c99
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-08 13:30:57 +0530

    [KhushiAgrawal] Added : CurrentAccount class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/Helper_Methods/CurrentAccount.java
@@ -0,0 +1,14 @@
+package com.Helper_Methods;
+
+public class CurrentAccount extends Account {
+    private double overdraftLimit;
+
+    public CurrentAccount(int accountNumber, double balance, double overdraftLimit) {
+        super(accountNumber, balance);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    public double getOverdraftLimit() {
+        return overdraftLimit;
+    }
+}
