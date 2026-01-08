669d07192ea12719aaf4cfdcd0e078db0c225e8d
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-08 14:31:25 +0530

    [KhushiAgrawal] Added : Conference Event class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/EventEase_Event_Management_Platform/ConferenceEvent.java
@@ -0,0 +1,16 @@
+package com.EventEase_Event_Management_Platform;
+
+public class ConferenceEvent extends Event {
+
+    public ConferenceEvent(int id, String name, String location, String date,
+                            int attendees, boolean catering) {
+        super(id, name, location, date, attendees, 10000);
+        if (catering) updateCost(5000);
+    }
+
+    @Override
+    public void schedule() {
+        System.out.println("Conference scheduled 🏢");
+    }
+}
+
