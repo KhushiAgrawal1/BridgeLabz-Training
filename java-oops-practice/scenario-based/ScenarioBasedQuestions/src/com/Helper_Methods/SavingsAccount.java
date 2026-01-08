3df150e287ebd149a0cb395e5a8192db490143d9
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-08 13:32:04 +0530

    [KhushiAgrawal] Added : SavingsAccount class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/Helper_Methods/SavingsAccount.java
@@ -0,0 +1,15 @@
+package com.Helper_Methods;
+
+public class SavingsAccount extends Account {
+    private double interestRate;
+
+    public SavingsAccount(int accountNumber, double balance, double interestRate) {
+        super(accountNumber, balance);
+        this.interestRate = interestRate;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+}
+
