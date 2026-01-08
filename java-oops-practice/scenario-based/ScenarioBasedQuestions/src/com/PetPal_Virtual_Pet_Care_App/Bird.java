80592591f4338d87b0b1e4dc7ed1b4cdf8291c41
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-08 14:50:07 +0530

    [KhushiAgrawal] Added : Bird class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/PetPal_Virtual_Pet_Care_App/Bird.java
@@ -0,0 +1,29 @@
+package com.PetPal_Virtual_Pet_Care_App;
+
+public class Bird extends Pet implements IInteractable {
+
+    public Bird(String name, int age) {
+        super(name, "Bird", age);
+    }
+
+    public void feed() {
+        changeHunger(-10);
+        System.out.println("Bird is eating");
+    }
+
+    public void play() {
+        changeEnergy(-15);
+        System.out.println("Bird is flying");
+    }
+
+    public void sleep() {
+        changeEnergy(30);
+        System.out.println("Bird is sleeping");
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Bird says: Chirp!");
+    }
+}
+
