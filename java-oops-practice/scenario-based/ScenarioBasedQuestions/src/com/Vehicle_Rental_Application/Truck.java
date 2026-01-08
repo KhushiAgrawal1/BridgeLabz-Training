141c6570ba27bb31bf7c3436c52f57074bff2baf
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-08 15:55:58 +0530

    [KhushiAgrawal] Added : Truck class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/Vehicle_Rental_Application/Truck.java
@@ -0,0 +1,13 @@
+package com.Vehicle_Rental_Application;
+
+public class Truck extends Vehicle implements Rentable {
+
+    public Truck(String number) {
+        super(number, 1200);
+    }
+
+    public double calculateRent(int days) {
+        return (baseRate * days) + 1000;
+    }
+}
+
