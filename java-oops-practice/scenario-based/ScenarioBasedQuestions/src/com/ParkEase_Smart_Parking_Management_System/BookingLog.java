88f6fb967151f06d3c7ef777c9db64b3dfc903c7
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-08 14:45:31 +0530

    [KhushiAgrawal] Added : BookingLog class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/ParkEase_Smart_Parking_Management_System/BookingLog.java
@@ -0,0 +1,14 @@
+package com.ParkEase_Smart_Parking_Management_System;
+
+public class BookingLog {
+    private String logMessage;
+
+    public BookingLog(String logMessage) {
+        this.logMessage = logMessage;
+    }
+
+    public String getLogMessage() {
+        return logMessage;
+    }
+}
+
