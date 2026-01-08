8cf41304fb3c9beb2962c705afeea29cc3ea9759
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-08 14:43:50 +0530

    [KhushiAgrawal] Added : MyBankMain class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/MyBank_Managing_Customer_Accounts/MyBankMain.java
@@ -0,0 +1,18 @@
+package com.MyBank_Managing_Customer_Accounts;
+
+public class MyBankMain {
+    public static void main(String[] args) {
+
+        Account acc = new SavingsAccount("SB101", 10000); // Polymorphism
+        ITransaction txn = (ITransaction) acc;
+
+        txn.deposit(2000);
+        txn.withdraw(1000);
+
+        System.out.println("Balance: ₹" + txn.checkBalance());
+
+        SavingsAccount sa = (SavingsAccount) acc;
+        System.out.println("Interest: ₹" + sa.calculateInterest());
+    }
+}
+
