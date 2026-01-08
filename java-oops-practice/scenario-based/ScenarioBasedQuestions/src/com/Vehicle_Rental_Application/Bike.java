fa043f07efe2bdb2a15e3a0e8bc0b0eb4f411010
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-08 15:54:23 +0530

    [KhushiAgrawal] Added : Bike class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/Vehicle_Rental_Application/Bike.java
@@ -0,0 +1,13 @@
+package com.Vehicle_Rental_Application;
+
+public class Bike extends Vehicle implements Rentable {
+
+    public Bike(String number) {
+        super(number, 300);
+    }
+
+    public double calculateRent(int days) {
+        return baseRate * days;
+    }
+}
+
