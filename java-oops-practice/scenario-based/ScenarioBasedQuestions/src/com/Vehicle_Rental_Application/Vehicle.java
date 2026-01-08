e550a77e5aa1e16ef6cf360ff7c329fd77fd38d4
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-08 15:56:18 +0530

    [KhushiAgrawal] Added : Vehicle class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/Vehicle_Rental_Application/Vehicle.java
@@ -0,0 +1,12 @@
+package com.Vehicle_Rental_Application;
+
+public class Vehicle {
+    protected String number;
+    protected double baseRate;
+
+    public Vehicle(String number, double baseRate) {
+        this.number = number;
+        this.baseRate = baseRate;
+    }
+}
+
