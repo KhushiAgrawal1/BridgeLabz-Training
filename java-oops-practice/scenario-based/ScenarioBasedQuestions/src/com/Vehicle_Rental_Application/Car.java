6e19f27602fcd9747cdb048a887f637cd4bb2dea
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-08 15:55:18 +0530

    [KhushiAgrawal] Added : Car class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/Vehicle_Rental_Application/Car.java
@@ -0,0 +1,13 @@
+package com.Vehicle_Rental_Application;
+
+public class Car extends Vehicle implements Rentable {
+
+    public Car(String number) {
+        super(number, 800);
+    }
+
+    public double calculateRent(int days) {
+        return (baseRate * days) + 500;
+    }
+}
+
