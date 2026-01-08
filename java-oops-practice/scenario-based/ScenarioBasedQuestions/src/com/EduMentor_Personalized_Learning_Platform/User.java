adcc4022d3f906950564566bcfee686ca068c645
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-08 14:29:32 +0530

    [KhushiAgrawal] Added : User class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/EduMentor_Personalized_Learning_Platform/User.java
@@ -0,0 +1,14 @@
+package com.EduMentor_Personalized_Learning_Platform;
+
+public class User {
+    protected int userId;
+    protected String name;
+    protected String email;
+
+    public User(int userId, String name, String email) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+    }
+}
+
