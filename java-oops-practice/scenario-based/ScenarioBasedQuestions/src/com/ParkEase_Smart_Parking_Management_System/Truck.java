b8fcf683b464333dd6692fad415d223080f0f5da
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-08 14:48:48 +0530

    [KhushiAgrawal] Added : Truck class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/ParkEase_Smart_Parking_Management_System/Truck.java
@@ -0,0 +1,18 @@
+package com.ParkEase_Smart_Parking_Management_System;
+
+public class Truck extends Vehicle {
+
+    public Truck(String vehicleNumber) {
+        super(vehicleNumber, 80);
+    }
+
+    @Override
+    public double calculateCharges(int hours) {
+        double charge = baseRate * hours;
+        if (hours > 5) {
+            charge += 200;
+        }
+        return charge;
+    }
+}
+
