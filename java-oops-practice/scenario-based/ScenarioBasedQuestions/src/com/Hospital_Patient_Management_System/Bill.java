1708c9f628215386e5a4b074a921ee7bb41bc817
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-08 11:19:55 +0530

    [KhushiAgrawal] Added : Bill class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/Hospital_Patient_Management_System/Bill.java
@@ -0,0 +1,15 @@
+package com.Hospital_Patient_Management_System;
+
+public class Bill implements Payable {
+    private double amount;
+
+    public Bill(double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public double calculatePayment() {
+        double tax = amount * 0.05;
+        return amount + tax;
+    }
+}
