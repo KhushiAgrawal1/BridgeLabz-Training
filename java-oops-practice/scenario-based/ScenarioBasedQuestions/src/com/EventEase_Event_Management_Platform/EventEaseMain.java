1024064248273c3e73f215ed5b91c4a7c90ecd16
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-08 14:32:27 +0530

    [KhushiAgrawal] Added : EventEaseMain class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/EventEase_Event_Management_Platform/EventEaseMain.java
@@ -0,0 +1,16 @@
+package com.EventEase_Event_Management_Platform;
+
+public class EventEaseMain {
+    public static void main(String[] args) {
+
+        User organizer = new User("Khushi", "khushi@gmail.com");
+
+        Event event = new BirthdayEvent(
+                101, "Khushi's Birthday", "Indore",
+                "10-02-2026", 50, true);
+
+        event.schedule();                 // Polymorphism
+        System.out.println("Total Cost: ₹" + event.getCost());
+    }
+}
+
