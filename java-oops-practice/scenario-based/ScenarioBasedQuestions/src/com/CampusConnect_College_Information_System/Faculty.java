35b3fdde1b979a15eff3540abe25e0b34ad98b55
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-08 14:20:59 +0530

    [KhushiAgrawal] Added : Faculty class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/CampusConnect_College_Information_System/Faculty.java
@@ -0,0 +1,17 @@
+package com.CampusConnect_College_Information_System;
+
+public class Faculty extends Person {
+
+    private String department;
+
+    public Faculty(int id, String name, String email, String department) {
+        super(id, name, email);
+        this.department = department;
+    }
+
+    @Override
+    public void printDetails() {
+        System.out.println("Faculty: " + name + ", Dept: " + department);
+    }
+}
+
