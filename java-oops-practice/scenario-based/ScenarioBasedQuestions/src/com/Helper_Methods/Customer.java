a68fd71d22930792a4d4fdf7ff9b0bcf13f8d9fe
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-08 13:31:17 +0530

    [KhushiAgrawal] Added : Customer class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/Helper_Methods/Customer.java
@@ -0,0 +1,20 @@
+package com.Helper_Methods;
+
+public class Customer {
+    private String name;
+    private int customerId;
+
+    public Customer(String name, int customerId) {
+        this.name = name;
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+}
+
