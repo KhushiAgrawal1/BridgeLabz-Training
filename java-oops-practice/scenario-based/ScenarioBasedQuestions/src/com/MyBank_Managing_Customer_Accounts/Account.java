1ba2fa56f0c20eb08bdfbe515ca7ea406f2e2e51
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-08 14:42:15 +0530

    [KhushiAgrawal] Added : Account class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/MyBank_Managing_Customer_Accounts/Account.java
@@ -0,0 +1,29 @@
+package com.MyBank_Managing_Customer_Accounts;
+
+public class Account {
+    protected String accountNumber;
+    private double balance;
+
+    public Account(String accountNumber) {
+        this.accountNumber = accountNumber;
+        this.balance = 0;
+    }
+
+    public Account(String accountNumber, double openingBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = openingBalance;
+    }
+
+    protected void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    protected double getBalanceInternal() {
+        return balance;
+    }
+
+    public double checkBalance() {
+        return balance;
+    }
+}
+
