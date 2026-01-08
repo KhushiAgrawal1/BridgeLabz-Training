4aeaabe48d66860fce83b666bd4679dbc490bd89
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-08 14:28:42 +0530

    [KhushiAgrawal] Added : Learner class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/EduMentor_Personalized_Learning_Platform/Learner.java
@@ -0,0 +1,21 @@
+package com.EduMentor_Personalized_Learning_Platform;
+
+public class Learner extends User implements ICertifiable {
+
+    private String courseType; // Short / FullTime
+
+    public Learner(int id, String name, String email, String courseType) {
+        super(id, name, email);
+        this.courseType = courseType;
+    }
+
+    @Override
+    public void generateCertificate() {
+        if (courseType.equalsIgnoreCase("Short")) {
+            System.out.println("Short Course Certificate issued to " + name);
+        } else {
+            System.out.println("Full-Time Program Certificate issued to " + name);
+        }
+    }
+}
+
