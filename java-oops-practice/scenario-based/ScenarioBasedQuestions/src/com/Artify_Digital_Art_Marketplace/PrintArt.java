d417945f51650ff0ee5ed9c5561370f0f960e3fb
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-12 13:07:43 +0530

    [KhushiAgrawal] Added : PrintArt class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/Artify_Digital_Art_Marketplace/PrintArt.java
@@ -0,0 +1,10 @@
+package com.Artify_Digital_Art_Marketplace;
+
+class PrintArt extends Artwork{
+
+	public PrintArt(String title, String artist, int price, String licenseType, boolean hasPreview) {
+		super(title, artist, price, licenseType, hasPreview);
+
+	}
+	
+}
