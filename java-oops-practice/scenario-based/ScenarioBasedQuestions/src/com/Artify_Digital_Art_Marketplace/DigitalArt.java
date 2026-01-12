484f1b2862dafe36defcae50ba04b2193a9f5284
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-12 13:07:27 +0530

    [KhushiAgrawal] Added : DigitalArt class

---
+++ java-oops-practice/scenario-based/ScenarioBasedQuestions/src/com/Artify_Digital_Art_Marketplace/DigitalArt.java
@@ -0,0 +1,12 @@
+package com.Artify_Digital_Art_Marketplace;
+
+class DigitalArt extends Artwork{
+
+	public DigitalArt(String title, String artist, int price, String licenseType, boolean hasPreview) {
+		super(title, artist, price, licenseType, hasPreview);
+
+	}
+	
+}
+
+
