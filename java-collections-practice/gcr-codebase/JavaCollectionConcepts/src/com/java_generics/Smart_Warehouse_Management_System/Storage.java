3f5171fe872f5ec4e8c838dad3aafc8e82c4e4dc
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-15 13:38:36 +0530

    [KhushiAgrawal] Added : Storage class

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/java_generics/Smart_Warehouse_Management_System/Storage.java
@@ -0,0 +1,18 @@
+package com.java_generics.Smart_Warehouse_Management_System;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Storage<T extends WarehouseItem> {
+
+    private List<T> items = new ArrayList<>();
+
+    public void addItem(T item) {
+        items.add(item);
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+}
+
