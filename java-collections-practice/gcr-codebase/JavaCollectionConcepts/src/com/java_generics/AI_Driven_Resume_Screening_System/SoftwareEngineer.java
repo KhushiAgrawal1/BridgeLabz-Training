49061c9c9d3375667473f8160c95171254714996
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-18 21:23:46 +0530

    [KhushiAgrawal] Added : Software Engineer class

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/java_generics/AI_Driven_Resume_Screening_System/SoftwareEngineer.java
@@ -0,0 +1,14 @@
+package com.java_generics.AI_Driven_Resume_Screening_System;
+
+class SoftwareEngineer extends JobRole {
+
+    public SoftwareEngineer() {
+        super("Software Engineer");
+    }
+
+    @Override
+    public void evaluateResume() {
+        System.out.println("Evaluating coding skills and problem-solving ability");
+    }
+}
+
