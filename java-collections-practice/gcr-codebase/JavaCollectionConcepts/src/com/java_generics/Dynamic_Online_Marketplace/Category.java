90f74de12925a4e48e59cea51ecdde4e95e43181
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-18 21:02:51 +0530

    [KhushiAgrawal] Added: Category class

--- java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/java_generics/Dynamic_Online_Marketplace/Category.java
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/java_generics/Dynamic_Online_Marketplace/Category.java
@@ -1,5 +1,14 @@
 package com.java_generics.Dynamic_Online_Marketplace;
 
-public class Category {
+abstract class Category {
+    protected String categoryName;
 
+    public Category(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
 }
+
