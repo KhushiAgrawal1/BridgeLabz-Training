c8d3a0813e3db1b8bb9271c77d2d542bd741e46d
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-18 21:21:12 +0530

    [KhushiAgrawal] Added : Data Scientist class

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/java_generics/AI_Driven_Resume_Screening_System/DataScientist.java
@@ -0,0 +1,14 @@
+package com.java_generics.AI_Driven_Resume_Screening_System;
+
+class DataScientist extends JobRole {
+
+    public DataScientist() {
+        super("Data Scientist");
+    }
+
+    @Override
+    public void evaluateResume() {
+        System.out.println("Evaluating data analysis and machine learning skills");
+    }
+}
+
