f89bb3fcffff4aef37907d7a868cde6a9c3d0f81
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-21 16:08:37 +0530

    [KhushiAgrawal] Added : CheckSetEquality class

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/collections/set_interface/CheckSetEquality.java
@@ -0,0 +1,16 @@
+package com.collections.set_interface;
+
+import java.util.*;
+
+public class CheckSetEquality {
+
+    public static void main(String[] args) {
+        Set<Integer> set1 = new HashSet<>(Arrays.asList(1, 2, 3));
+        Set<Integer> set2 = new HashSet<>(Arrays.asList(3, 2, 1));
+
+        boolean isEqual = set1.equals(set2);
+
+        System.out.println("Are both sets equal? " + isEqual);
+    }
+}
+
