e6e655e0933761f6d73ca66ada9f3b4e74eb07a8
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-24 11:27:56 +0530

    [KhushiAgrawal] Added : Employee class

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/streams/serialization/Employee.java
@@ -0,0 +1,31 @@
+package com.streams.serialization;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    int id;
+    String name;
+    String department;
+    double salary;
+
+    // Constructor
+    public Employee(int id, String name, String department, double salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    // Display method
+    public void display() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Department: " + department);
+        System.out.println("Salary: " + salary);
+        System.out.println("------------------------");
+    }
+}
+
