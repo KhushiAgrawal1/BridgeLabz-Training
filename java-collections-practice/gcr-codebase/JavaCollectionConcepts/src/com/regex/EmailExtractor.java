1a2172d9c88bd0c8c6fd31a7c780b128b5f3db11
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-26 19:33:01 +0530

    [KhushiAgrawal] Added : EmailExtractor class

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/regex/EmailExtractor.java
@@ -0,0 +1,23 @@
+package com.regex;
+
+import java.util.regex.*;
+import java.util.*;
+
+public class EmailExtractor {
+    public static void main(String[] args) {
+
+        String text = "Contact us at support@example.com and info@company.org";
+
+        // Regex for email
+        String regex = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        // Find and print emails
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
+
