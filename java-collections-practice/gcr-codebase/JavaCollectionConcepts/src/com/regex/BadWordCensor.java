10a7ba34989db2045f01fe5545f6e277e652746d
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-27 19:43:49 +0530

    [KhushiAgrawal] Added : BadWordCensor class

--- java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/regex/BadWordCensor.java
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/regex/BadWordCensor.java
@@ -1,5 +1,19 @@
 package com.regex;
 
 public class BadWordCensor {
+	public static void main(String[] args) {
+
+        String sentence = "This is a damn bad example with some stupid words.";
+
+        // List of bad words
+        String[] badWords = {"damn", "stupid"};
+
+        for (String word : badWords) {
+            // Replace each bad word with ****
+            sentence = sentence.replaceAll("(?i)\\b" + word + "\\b", "****");
+        }
+
+        System.out.println(sentence);
+    }
 
 }
