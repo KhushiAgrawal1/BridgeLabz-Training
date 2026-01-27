3b676f09e46090211bd61f15474b27c417bf7cc8
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-27 19:16:16 +0530

    [KhushiAgrawal] Added : ServiceA class

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/reflection/advanced_level/CreateInjectAnnotation/ServiceA.java
@@ -0,0 +1,7 @@
+package com.reflection.advanced_level.CreateInjectAnnotation;
+
+public class ServiceA {
+    public void doService() {
+        System.out.println("ServiceA is executing...");
+    }
+}
