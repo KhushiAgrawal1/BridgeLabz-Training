4a9816eea9c49352c573cd0c6d6e075142aae31e
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-27 19:20:30 +0530

    [KhushiAgrawal] Added : Person class

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/reflection/advanced_level/GenerateaJSONRepresentation/Person.java
@@ -0,0 +1,14 @@
+package com.reflection.advanced_level.GenerateaJSONRepresentation;
+
+public class Person {
+    private String name;
+    private int age;
+    private boolean active;
+
+    public Person(String name, int age, boolean active) {
+        this.name = name;
+        this.age = age;
+        this.active = active;
+    }
+}
+
