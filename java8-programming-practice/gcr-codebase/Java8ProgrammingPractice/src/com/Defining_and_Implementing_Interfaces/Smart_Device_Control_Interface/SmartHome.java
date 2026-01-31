0840391d7d9c99281ed434e55bdcb46c8a93810c
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-31 19:21:23 +0530

    [KhushiAgrawal] Added : Home class

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingPractice/src/com/Defining_and_Implementing_Interfaces/Smart_Device_Control_Interface/SmartHome.java
@@ -0,0 +1,19 @@
+package com.Defining_and_Implementing_Interfaces.Smart_Device_Control_Interface;
+
+public class SmartHome {
+    public static void main(String[] args) {
+
+        SmartDevice light = new Light();
+        SmartDevice ac = new AC();
+        SmartDevice tv = new TV();
+
+        light.turnOn();
+        ac.turnOn();
+        tv.turnOn();
+
+        light.turnOff();
+        ac.turnOff();
+        tv.turnOff();
+    }
+}
+
