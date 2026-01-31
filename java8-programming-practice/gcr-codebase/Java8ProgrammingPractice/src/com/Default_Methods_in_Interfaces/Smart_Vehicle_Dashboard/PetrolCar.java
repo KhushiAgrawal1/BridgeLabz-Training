82e66ab2f54f4180b68f80e9f9f95c5b9894652b
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-31 19:29:52 +0530

    [KhushiAgrawal] Added : PetrolCar class

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingPractice/src/com/Default_Methods_in_Interfaces/Smart_Vehicle_Dashboard/PetrolCar.java
@@ -0,0 +1,9 @@
+package com.Default_Methods_in_Interfaces.Smart_Vehicle_Dashboard;
+
+class PetrolCar implements Vehicle {
+
+    public void displaySpeed() {
+        System.out.println("Speed: 80 km/h");
+    }
+}
+
