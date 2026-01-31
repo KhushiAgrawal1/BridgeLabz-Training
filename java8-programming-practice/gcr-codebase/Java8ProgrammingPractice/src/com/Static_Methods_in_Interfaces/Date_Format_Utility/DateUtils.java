2e9859b9ec8fd21b836e00a12a251e116583a1a3
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-31 19:35:49 +0530

    [KhushiAgrawal] Added : DateUtils class

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingPractice/src/com/Static_Methods_in_Interfaces/Date_Format_Utility/DateUtils.java
@@ -0,0 +1,12 @@
+package com.Static_Methods_in_Interfaces.Date_Format_Utility;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+interface DateUtils {
+
+    static String formatDate(LocalDate date, String pattern) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        return date.format(formatter);
+    }
+}
