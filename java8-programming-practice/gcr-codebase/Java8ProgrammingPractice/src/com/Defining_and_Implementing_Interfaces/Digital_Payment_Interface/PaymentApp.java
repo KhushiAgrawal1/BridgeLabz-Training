4179f315c258a095cf3c4e35558f9ca76b7d3f46
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-31 19:16:37 +0530

    [KhushiAgrawal] Added : PaymentApp class

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingPractice/src/com/Defining_and_Implementing_Interfaces/Digital_Payment_Interface/PaymentApp.java
@@ -0,0 +1,14 @@
+package com.Defining_and_Implementing_Interfaces.Digital_Payment_Interface;
+
+public class PaymentApp {
+    public static void main(String[] args) {
+
+        Payment p1 = new UPI();
+        Payment p2 = new CreditCard();
+        Payment p3 = new Wallet();
+
+        p1.pay(500);
+        p2.pay(1200);
+        p3.pay(300);
+    }
+}
