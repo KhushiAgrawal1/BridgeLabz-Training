9be3b62f29e705c0ae360f83ccabd6babb7e597b
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-31 14:47:32 +0530

    [KhushiAgrawal] Added : Patient class

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingPractice/src/method_references/Hospital_Patient_ID_Printing/Patient.java
@@ -0,0 +1,14 @@
+package method_references.Hospital_Patient_ID_Printing;
+
+class Patient {
+    int id;
+
+    Patient(int id) {
+        this.id = id;
+    }
+
+    int getId() {
+        return id;
+    }
+}
+
