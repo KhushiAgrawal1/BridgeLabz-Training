81a31377888382dc000cea3c9fbc22a98acc32d4
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-01-31 19:38:08 +0530

    [KhushiAgrawal] Added : SecurityUtils class

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingPractice/src/com/Static_Methods_in_Interfaces/Password_Strength_Validator/SecurityUtils.java
@@ -0,0 +1,23 @@
+package com.Static_Methods_in_Interfaces.Password_Strength_Validator;
+
+interface SecurityUtils {
+
+    static boolean isStrongPassword(String password) {
+
+        if (password == null || password.length() < 8) {
+            return false;
+        }
+
+        boolean hasUpper = false;
+        boolean hasLower = false;
+        boolean hasDigit = false;
+
+        for (char ch : password.toCharArray()) {
+            if (Character.isUpperCase(ch)) hasUpper = true;
+            else if (Character.isLowerCase(ch)) hasLower = true;
+            else if (Character.isDigit(ch)) hasDigit = true;
+        }
+
+        return hasUpper && hasLower && hasDigit;
+    }
+}
