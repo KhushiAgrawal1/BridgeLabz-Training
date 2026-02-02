b982e43544ffda963c9af99c75bd237e8c1ac633
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-02-02 13:01:08 +0530

    [KhushiAgrawal] Added : InsuranceClaim class

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingPractice/src/com/Stream_API/Insurance_Claim_Analysis/InsuranceClaim.java
@@ -0,0 +1,11 @@
+package com.Stream_API.Insurance_Claim_Analysis;
+
+class InsuranceClaim {
+    String claimType;
+    double claimAmount;
+
+    InsuranceClaim(String claimType, double claimAmount) {
+        this.claimType = claimType;
+        this.claimAmount = claimAmount;
+    }
+}
