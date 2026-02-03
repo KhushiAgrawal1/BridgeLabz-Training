e0ac73432822c49bce03c87f530c64a1b92f49a4
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-02-03 15:10:45 +0530

    [KhushiAgrawal] Added : Student class

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingPractice/src/com/Collectors/Student_Result_Grouping/Student.java
@@ -0,0 +1,19 @@
+package com.Collectors.Student_Result_Grouping;
+
+public class Student {
+	String name;
+	String grade;
+	public Student(String name, String grade) {
+		this.name = name;
+		this.grade = grade;
+	}
+
+
+	public String getName() {
+        return name;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+}
