127b84d10cb8b20a95629b5c2c54ee4def9b31f0
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-02-03 15:09:26 +0530

    [KhushiAgrawal] Added : Order class

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingPractice/src/com/Collectors/Order_Revenue_Summary/Order.java
@@ -0,0 +1,13 @@
+package com.Collectors.Order_Revenue_Summary;
+
+class Order {
+
+    String customer;
+    double amount;
+
+    Order(String customer, double amount) {
+        this.customer = customer;
+        this.amount = amount;
+    }
+}
+
