30349b6cc5c10a7b64ba32eb9afe3bcff17fa746
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-02-06 16:01:47 +0530

    [KhushiAgrawal] Added : Student class

---
+++ java-design-patterns-practice/gcr-codebase/JavaDesignPatternsPractice/src/com/design_patterns/Smart_University_Library_Management_System/Student.java
@@ -0,0 +1,18 @@
+package com.design_patterns.Smart_University_Library_Management_System;
+
+public class Student implements User {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public void showRole() {
+        System.out.println(name + " is a Student.");
+    }
+
+    public void update(String message) {
+        System.out.println(name + " notified: " + message);
+    }
+}
+
