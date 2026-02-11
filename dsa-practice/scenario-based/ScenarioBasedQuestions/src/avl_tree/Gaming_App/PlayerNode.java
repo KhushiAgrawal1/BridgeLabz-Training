c1a4f92c5808c9a33f6165fc0ef71c2371225fe3
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-02-11 09:58:03 +0530

    [KhushiAgrawal] Added : PlayerNode class

---
+++ dsa-practice/scenario-based/ScenarioBasedQuestions/src/avl_tree/Gaming_App/PlayerNode.java
@@ -0,0 +1,15 @@
+package avl_tree.Gaming_App;
+
+public class PlayerNode {
+    int score;
+    String name;
+    int height;
+    PlayerNode left, right;
+
+    public PlayerNode(int score, String name) {
+        this.score = score;
+        this.name = name;
+        this.height = 1;
+    }
+}
+
